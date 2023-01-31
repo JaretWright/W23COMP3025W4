0a660d971c5416dc60930066a9e47dfbf9e22883
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-31 15:33:25 -0500

    Added ViewBinding

--- app/src/main/java/com/constantlearningdad/w23comp3025w4/MainActivity.kt
+++ app/src/main/java/com/constantlearningdad/w23comp3025w4/MainActivity.kt
@@ -2,10 +2,16 @@ package com.constantlearningdad.w23comp3025w4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.constantlearningdad.w23comp3025w4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
