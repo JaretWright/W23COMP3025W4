354135741c0e99adfcaabfa4057df76a3316fcd0
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-31 16:27:57 -0500

    Added the intent to move from the main acitivity to the PassDetailsActivity.kt

--- app/src/main/java/com/constantlearningdad/w23comp3025w4/MainActivity.kt
+++ app/src/main/java/com/constantlearningdad/w23comp3025w4/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.constantlearningdad.w23comp3025w4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.constantlearningdad.w23comp3025w4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //when the button is clicked, make sure we have some text to
+        //pass to the next activity
+        binding.passDetailsButton.setOnClickListener {
+            var name = binding.nameEditText.text.toString()
 
+            //if the name is not populated, inform the user
+            if (name.isNotEmpty())
+            {
+                //pass the information to the PassDetailsActivity
+                //by creating an explicit intent
+                var intent = Intent(this, PassDetailsActivity::class.java)
+                intent.putExtra("name",name)
+                startActivity(intent)
+            }
+            else
+            {
+                Toast.makeText(this,"Enter your name",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/constantlearningdad/w23comp3025w4/PassDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.constantlearningdad.w23comp3025w4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PassDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pass_details)
+    }
+}
