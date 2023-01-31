9f6d3f29e33d49b601913ecc9ee39ec8733861d8
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-31 16:47:13 -0500

    added new layout for the web activity

--- app/src/main/java/com/constantlearningdad/w23comp3025w4/PassDetailsActivity.kt
+++ app/src/main/java/com/constantlearningdad/w23comp3025w4/PassDetailsActivity.kt
@@ -1,11 +1,23 @@
 package com.constantlearningdad.w23comp3025w4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.constantlearningdad.w23comp3025w4.databinding.ActivityPassDetailsBinding
 
 class PassDetailsActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityPassDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pass_details)
+        binding = ActivityPassDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var name = intent.getStringExtra("name")
+        binding.helloTextView.text = "Hello $name"
+
+        binding.passInfoBackToMain.setOnClickListener {
+            finish()
+        }
     }
 }
