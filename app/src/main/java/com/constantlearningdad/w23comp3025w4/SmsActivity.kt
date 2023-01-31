5074d007ed0adaaac0db561c25b6fb14f382620a
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-31 17:16:10 -0500

    Added the layout for the sms activity

---
+++ app/src/main/java/com/constantlearningdad/w23comp3025w4/SmsActivity.kt
@@ -0,0 +1,11 @@
+package com.constantlearningdad.w23comp3025w4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SmsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sms)
+    }
+}
